c8183925134c1752e84c1600a9e0f2e5003760d8
Mantosh Kumar <mantoshkichni@gmail.com>
2021-09-26 17:27:39 +0530

    init commit2

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const hbs=require('hbs');
 const path=require('path');
 const geocode=require('../utils/app.js')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const viewpath=path.join(__dirname,'../templates/views')
 const publidir=path.join(__dirname,'../public')
